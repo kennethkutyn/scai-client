e0078b3824152ea25b3d6356d0c8defd9a274da6
Kenneth Kutyn <kennethkutyn@gmail.com>
2023-03-28 14:50:25 +0800

    skipping proxy

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const prompt = "Hello, how are you?";
 const apiKey = "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";
 
-fetch(" https://scai.herokuapp.com/", {
+fetch("https://api.openai.com/v1/completions", {
   method: "POST",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json", 
+    "Authorization": 'Bearer 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'
   },
   body: JSON.stringify({
     "model": "text-davinci-003",
