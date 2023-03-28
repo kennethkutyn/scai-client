4e77eb90ba99d93bc809e28e1c4598c507168439
Kenneth Kutyn <kennethkutyn@gmail.com>
2023-03-28 15:03:54 +0800

    new key

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ fetch("https://api.openai.com/v1/completions", {
   method: "POST",
   headers: {
     "Content-Type": "application/json", 
-    "Authorization": 'Bearer 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'
+    "Authorization": 'Bearer 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'
   },
   body: JSON.stringify({
     "model": "text-davinci-003",
