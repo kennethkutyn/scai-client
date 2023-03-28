cc176288ade0d5d442e6832be5e1dda8bcfe8f51
Kenneth Kutyn <kennethkutyn@gmail.com>
2023-03-28 14:54:34 +0800

    skipping proxy3

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ fetch("https://api.openai.com/v1/completions", {
   method: "POST",
   headers: {
     "Content-Type": "application/json", 
-    "Authorization": 'Bearer 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'
+    "Authorization": 'Bearer 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'
   },
   body: JSON.stringify({
     "model": "text-davinci-003",
