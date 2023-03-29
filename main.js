d63c7e2514a8c44a93ef0bb7b3c3cc5ec036f031
Kenneth Kutyn <kennethkutyn@gmail.com>
2023-03-29 10:25:41 +0800

    trying again

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 const prompt = "Hello, how are you?";
 const apiKey = "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";
 
-fetch("https://api.openai.com/v1/completions", {
+
+
+function submitForm() {
+  let selectedOption;
+  if (document.getElementById('option1').checked) {
+    role = document.getElementById('option1').value;
+  } else if (document.getElementById('option2').checked) {
+    role = document.getElementById('option2').value;
+  } else if (document.getElementById('option3').checked) {
+    role = document.getElementById('option3').value;
+  } else {
+    role = document.getElementById('other-text').value;
+  }
+
+  const companyNameInput = document.getElementById('company-name-input').value;
+  const companyLocationInput = document.getElementById('company-location-input').value;
+
+  console.log('Prospect Role', role);
+  console.log('Company Name:', companyNameInput);
+  console.log('Company Location', companyLocationInput);
+
+  const prompt = "write a briefing doc for a sales person at Amplitude Analytics. They are going to a meeting with a " + role + " at " + companyNameInput + ". The doc should includ several sections: 1. The main concerns and focuses of a person in this role. 2. The Amplitude functionality that may be most relevant to this person. 3. Value selling consdierations - how to map Amplitude solutions to value drivers this person cares about."
+
+  fetch("https://scai.herokuapp.com/", {
   method: "POST",
   headers: {
     "Content-Type": "application/json", 
-    "Authorization": 'Bearer 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'
   },
   body: JSON.stringify({
     "model": "text-davinci-003",
@@ -17,5 +39,5 @@ fetch("https://api.openai.com/v1/completions", {
 .then(response => response.json())
 .then(data => console.log(data));
 
-
+}
 
