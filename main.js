9954f3b8ebe48eb8ebef45ee81aa7175c603ab06
Kenneth Kutyn <kennethkutyn@gmail.com>
2023-03-29 14:54:56 +0800

    improving prompt

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ function submitForm() {
                   '1. company background - 2-3 sentences on what ' + companyNameInput + ' does, their products, and their business model' +
                   '2. Competitors - list ' + companyNameInput + '  top 3 competitors' +
                   '3. Priorities for ' + role + ' - 3 sentences on the likely focus/priorities for this person in this company' +
-                  '4. Amplitude use cases - Identify the top 3 use cases for Amplitude product that might interest this person' +
-                  '5. Objections - Identify 3 objections this person might have' +
+                  '4. Amplitude use cases - Identify the top 3 use cases for Amplitude product that might interest the ' +role +' at ' + companyNameInput +
+                  '5. Objections - Identify 3 objections the ' + role + ' might have' +
                   '6. Value selling - identify 3 key ways Amplitude can drive value for this persona and company, specifically as it relates to analysis of their customer experiences';
 
   document.getElementById('input-form').remove()
