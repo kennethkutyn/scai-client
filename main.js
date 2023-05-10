995f7b741639d221264e610da52284daf48c8011
kennethkutyn <kennethkutyn@gmail.com>
2023-05-10 18:29:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function submitForm() {
   body: JSON.stringify({
           model: "gpt-3.5-turbo",
           messages: [
-            {"role": "user", "content": "hello"}
+            {"role": "user", "content": prompt}
           ],
           max_tokens: 1000,
           temperature: 0.7
