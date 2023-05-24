a61aed000d347b33329a3e98098e15382c8f9eb2
Kenneth Kutyn <kennethkutyn@gmail.com>
2023-05-24 12:54:03 +0800

    ksjld

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var numResponses = 0;
+
 function submitForm() {
   let selectedOption;
   if (document.getElementById('option1').checked) {
@@ -16,10 +18,8 @@ function submitForm() {
 
   const companyNameInput = document.getElementById('company-name-input').value;
 
-  var numResponses = 0;
-
-  console.log('Prospect Role:', role);
-  console.log('Company Name:', companyNameInput);
+  document.getElementById("input").style.display = "none";
+  document.getElementById("loading").style.display = "block";
 
   getNews(companyNameInput);
 
@@ -27,13 +27,14 @@ function submitForm() {
   const promptArray = [
       [
         'Provide a high level overview (2-3 sentences) on the company background of ' + companyNameInput + ', their products, and their business model, and if they have a parent company', 
-        'List 2-3 trends in  ' + companyNameInput + 's industry',
-        'list ' + companyNameInput + '  top 3 direct competitors, and mention if there is a well-known internation company with a similar business model',
-        'Whare are likely priorities for the ' + role + ' at ' + companyNameInput + ' - 3 sentences on the likely focus/priorities for this person in this company',
+        'List 2-3 trends in  ' + companyNameInput + 's industry, in numbered bullet points. No need for extra introductory text.',
+        'list ' + companyNameInput + '  top 3 direct competitors in numbered bullet points, and mention if there is a well-known internation company with a similar business model',
+        'Whare are likely priorities for the ' + role + ' at ' + companyNameInput + ' - 3 sentences on the likely focus/priorities for this person in this company, in numbered bullet points',
         'Brief me on ' + companyNameInput + 's digital innovation strategy',
-        'Brifely list the technology stack and software products that ' + companyNameInput + '  uses to build their products and caputure, store, and analyze user behaviour. State how certain you are or if you are making a guess based on similar companies.',
-        'Identify the top 3 use cases for Amplitude products that might interest the ' +role +' at ' + companyNameInput + 'and specifically mention Amplitude features that will address those use cases.',
-        'Identify 3 key ways Amplitude can drive value for ' + role + ' at ' + companyNameInput + ', specifically as it relates to analysis of their customer experiences'
+        'Brifely list the technology stack and software products that ' + companyNameInput + '  uses to build their products and caputure, store, and analyze user behaviour. List in numbered bullet points please. ',
+        'Provide 3 open-ended discovery questions that a salesperson at Amplitude might use to better understand the opportunity for Amplitude products to be used at ' + companyNameInput + ' by the ' + role,
+        'Identify the top 3 use cases in numbered bullet points for Amplitude products that might interest the ' +role +' at ' + companyNameInput + 'and specifically mention Amplitude features that will address those use cases.',
+        'Identify 3 key ways Amplitude can drive value for ' + role + ' at ' + companyNameInput + ', specifically as it relates to analysis of their customer experiences. Format your response in numbered bullet points.'
       ],
       [
         "Company Background",
@@ -42,24 +43,11 @@ function submitForm() {
         "Priorities",
         "Digital Innovation Strategy",
         "Tech Stack",
+        "Discovery",
         "Use Cases",
         "Value Selling"
       ]
   ]
-
-  
-  document.getElementById('input-form').remove();
-  document.getElementById('app-description').remove()
-  // Get the element with ID "Main"
-  const mainElement = document.getElementById("main");
-  // Create a new h1 element
-  const loadingElement = document.createElement("h1");
-  // Set the inner text to "loading"
-  loadingElement.innerText = "Loading. This will take about 30 seconds.";
-  // Set the ID attribute to "loading"
-  loadingElement.setAttribute("id", "loading");
-  // Append the h1 element to the main element
-  mainElement.appendChild(loadingElement);
 
   for (let i=0; i<promptArray[0].length; i++){
     makeCall(promptArray[0][i], promptArray[1][i]);
@@ -68,9 +56,13 @@ function submitForm() {
 }
 
 function responseReady(data, section) {
-  console.log(data);
+  numResponses = numResponses + 1;
+  if (numResponses = 6){
+    document.getElementById("response-content").style.display = "block";
+    document.getElementById("loading").style.display = "none";
+  }
 
-  document.getElementById(section).innerHTML = data;
+  document.getElementById(section).innerText = data;
 
   //mainElement.style.display = "none";
 
@@ -88,7 +80,6 @@ function responseReady(data, section) {
 
 
 function toggleContent(container) {
-  console.log(container);
   container.classList.toggle('expanded');
   const arrow = container.querySelector('.arrow');
   arrow.innerHTML = container.classList.contains('expanded') ? '&#x25BC;' : '&#x25B6;';
@@ -131,18 +122,10 @@ function getNews(company){
 }
 
 function handleNews(data){
-  console.log(data);
-  document.getElementById('loading').remove();
-
-  const mainElement = document.getElementById("main");
-  mainElement.style.display = "block";
-  document.getElementById("helpful-links").style.display = "block";
-  mainElement.style.display = "block";
-  document.getElementById("linkURL1").innerHTML = data.news[0].title;
-  document.getElementById("linkURL2").innerHTML = data.news[1].title;
-  document.getElementById("linkURL3").innerHTML = data.news[2].title;
+  document.getElementById("linkURL1").innerHTML = data.news[0].title.slice(0, 90);
+  document.getElementById("linkURL2").innerHTML = data.news[1].title.slice(0, 90);
+  document.getElementById("linkURL3").innerHTML = data.news[2].title.slice(0, 90);
   document.getElementById("linkURL1").href = data.news[0].url;
   document.getElementById("linkURL2").href = data.news[1].url;
   document.getElementById("linkURL3").href = data.news[2].url;
-  document.getElementById("news").style.display = "block";
 }
